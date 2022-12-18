7bda8d7d5b79ce4d1a8b27e41108c860e2da561a
Diego Macias <djoromac@hotmail.com>
2022-12-18 18:06:27 -0500

    added Mobile Menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 const emailMenu = document.querySelector('.nav--email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamburguerMenu = document.querySelector('.nav--menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 const toggleDesktopMenu = () => {
     desktopMenu.classList.toggle('inactive');
 }
 
+const toggleMobileMenu = () =>{
+    mobileMenu.classList.toggle('inactive');
+}
+
 emailMenu.addEventListener('click', toggleDesktopMenu);
+hamburguerMenu.addEventListener('click', toggleMobileMenu);
+
