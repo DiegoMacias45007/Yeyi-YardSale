82fb691dc30c84b69af41fff5f12606afe110654
Diego Macias <djoromac@hotmail.com>
2022-12-22 15:22:01 -0500

    added change cart icon in js

--- js/main.js
+++ js/main.js
@@ -72,6 +72,51 @@ productList.push({
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPYv8JB7LqsalIwyI0SoIrddSYLE45M4xqhQ&usqp=CAU'
 })
 
+productList.push ({
+    name:'Bike',
+    price: 12700,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1200,
+    image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1600,
+    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1500,
+    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+    name:'Seat',
+    price: 300,
+    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
+});
+productList.push ({
+    name:'Tennis Montain Bike',
+    price: 2200,
+    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+});
+productList.push ({
+    name:'Sunglasses',
+    price: 800,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
+});
+productList.push ({
+    name:'Sunglasses',
+    price: 600,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
+});
+productList.push ({
+    name:'Bicycle seat bag',
+    price: 876,
+    image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
+}); 
 // Existen dos formas en las que podemos recorrer un array para introducir cada producto al HTML
 
 // for(i of array)
@@ -104,6 +149,7 @@ const renderProducts = (arr) => {
         itemCart.setAttribute('name', 'Add to Cart');
         itemCart.setAttribute('title', 'Add to Cart');
         itemCart.addEventListener('click', () => {addToCart(i, itemCart)})
+        product.cart = itemCart;
     
         shopItem.append(productImage, itemPrice, itemTitle, itemCart);
     
@@ -167,6 +213,7 @@ const removeFromCart = (element1, element2) =>{
     let index = cartProducts.indexOf(element2);
     cartProducts.splice(index, 1);
     element1.remove();
+    element2.cart.classList.remove('item--added');
     sumTotal();
     if(cartProducts.length === 0){
         emptyCart.classList.remove('inactive');
